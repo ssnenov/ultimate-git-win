0f0ccbc35f30e2c6f8f6378576fc44089a5c2aba
Simeon Nenov <nenov.simeon@gmail.com>
2018-07-11 18:50:01 +0300

    Add gzip compression in forecast plugin

--- forecast/ultimate-git-win-forecast.ps1
+++ forecast/ultimate-git-win-forecast.ps1
@@ -11,7 +11,8 @@ if (-not $useCelsius) {
 
 $URI = "https://query.yahooapis.com/v1/public/yql?q=select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text='{0}, {1}') and u='{2}' &format=json&env=store://datatables.org/alltableswithkeys" -f $city, $countryCode, $tempScale
 
-$Data = Invoke-RestMethod -Uri  $URI -TimeoutSec 2 -ErrorAction Stop
+$headers = @{ "Accept-Encoding" = "gzip" }
+$Data = Invoke-RestMethod -Uri  $URI -TimeoutSec 2 -Headers $headers -ErrorAction Stop
 
 $Data.query.results.channel.item.condition[0].temp
 $Data.query.results.channel.item.condition[0].text
